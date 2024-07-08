83355280330e48b54ea90fe815413c30f3326cf2
Barasa <timbaros87@gmail.com>
2024-07-08 21:12:42 +0300

    Input Output complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-   #Add .txt extension
+    # Convert file_name to string if it is a PosixPath object
+    if isinstance(file_name, Path):
+        file_name = str(file_name)
+        
+    # Add .txt extension
     if not file_name.endswith('.txt'):
         file_name += '.txt'
     
@@ -7,8 +13,26 @@ def write_file(file_name, file_content):
     with open(file_name, 'w') as file:
         file.write(file_content)
 
+
 def append_file(file_name, append_content):
-    pass
+    # Convert file_name to string if it is a PosixPath object
+    if isinstance(file_name, Path):
+        file_name = str(file_name)
+        
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+
+    with open(file_name, 'a') as file:
+        file.write(append_content)
+    
 
 def read_file(file_name):
-    pass
+    # Convert file_name to string if it is a PosixPath object
+    if isinstance(file_name, Path):
+        file_name = str(file_name)
+        
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+
+    with open(file_name, 'r') as file:
+        return file.read()
